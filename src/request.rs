c15c7e6b230479d773a4fdb00d3eadff1cfb0f56
Daniel Wilk <wilkdaniel03@gmail.com>
2022-11-28 14:15:37 +0100

    [request] Add is_https and is_http functions

--- src/request.rs
+++ src/request.rs
@@ -1,1 +1,39 @@
-pub struct Request;
+pub struct Request {
+    is_secured: bool
+}
+
+fn is_https(url: &str) -> bool {
+    url.contains("https://")
+}
+
+fn is_http(url: &str) -> bool {
+    url.contains("http://")
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const SECURED_URL: &str = "https://xyz.com";
+    const INSECURED_URL: &str = "http://xyz.com";
+
+    #[test]
+    fn test_is_https_returns_true() {
+        assert_eq!(is_https(&SECURED_URL), true)
+    }
+
+    #[test]
+    fn test_is_https_returns_false() {
+        assert_eq!(is_https(&INSECURED_URL), false)
+    }
+
+    #[test]
+    fn test_is_http_returns_true() {
+        assert_eq!(is_http(&INSECURED_URL), true)
+    }
+
+    #[test]
+    fn test_is_http_returns_false() {
+        assert_eq!(is_http(&SECURED_URL), false)
+    }
+}
