0528b0e0729b396a67155a357f969c9ac1f1cc3f
Daniel Wilk <wilkdaniel03@gmail.com>
2022-11-28 14:39:15 +0100

    [request] Check protocol in request's constructor

--- src/request.rs
+++ src/request.rs
@@ -1,3 +1,4 @@
+#[derive(Debug, Clone, PartialEq)]
 pub struct Request {
     is_secured: bool
 }
@@ -10,12 +11,28 @@ fn is_http(url: &str) -> bool {
     url.contains("http://")
 }
 
+impl Request {
+    pub fn new(url: &str) -> Self {
+        if is_https(url) {
+            Self { is_secured: true }
+        } else if is_http(url) {
+            Self { is_secured: false }
+        } else {
+            panic!("Invalid protocol in served url")
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     const SECURED_URL: &str = "https://xyz.com";
     const INSECURED_URL: &str = "http://xyz.com";
+    const FTP_URL: &str = "ftp://xyz.com";
+
+    const SECURED_REQUEST: Request = Request { is_secured: true };
+    const INSECURED_REQUEST: Request = Request { is_secured: false };
 
     #[test]
     fn test_is_https_returns_true() {
@@ -35,5 +52,23 @@ mod tests {
     #[test]
     fn test_is_http_returns_false() {
         assert_eq!(is_http(&SECURED_URL), false)
+    }
+
+    #[test]
+    fn test_construct_https_request() {
+        let req = Request::new(SECURED_URL);
+        assert_eq!(req, SECURED_REQUEST)
+    }
+
+    #[test]
+    fn test_construct_http_request() {
+        let req = Request::new(INSECURED_URL);
+        assert_eq!(req, INSECURED_REQUEST)
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_construct_ftp_request() {
+        let _req = Request::new(FTP_URL);
     }
 }
